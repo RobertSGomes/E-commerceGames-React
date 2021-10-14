853fb3cbc474d3172cd8e572af188d6a23ca60f6
RobertSGomes <robert.d.s.g@hotmail.com>
2021-10-14 17:23:51 -0300

    Backend adicionado e ReadMe atualizado

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./app/controllers/index')(app);
+
+app.listen(8000, () => {
+    console.log('porta 8000 iniciada');
+});
